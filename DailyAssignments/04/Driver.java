9fdcab8575c7bd33a220c1d92452437f0cb4d6fc
Joshua Hans <mrhansmath@gmail.com>
2020-07-06 15:03:54 -0400

    more test cases

--- DailyAssignments/04/Driver.java
+++ DailyAssignments/04/Driver.java
@@ -8,8 +8,18 @@ public class Driver{
 
     arr2.add(3);
     arr2.add(5);
-    System.out.println(arr2.toString());
+    arr2.add(10);
+    System.out.println(arr2);
 
+    arr2.grow(2);
+    arr2.add(6);
 
+    System.out.println(arr2);
+
+    arr2.remove(2);
+    System.out.println(arr2);
+
+    arr2.add(1, 10);
+    System.out.println(arr2);
   }//end main
 }//end class
