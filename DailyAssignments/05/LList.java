064883eb42f701abb6e1262730701ec2c9e345fd
Joshua Hans <mrhansmath@gmail.com>
2020-07-08 14:34:30 -0400

    refactored get, set, search

--- DailyAssignments/05/LList.java
+++ DailyAssignments/05/LList.java
@@ -1,50 +1,67 @@
 import java.io.*;
 import java.util.*;
 
-
 public class LList{
   private Node head;
+  private int length;
 
   public LList(){
-    head = null;
+    this.head = null; //empty linked list
+    this.length = 0;
   }//end constructor method
 
   public void addFront(String data){
     Node newnode = new Node(data);
     newnode.setNext(head); // first point the new node to the beginning
-    head = newnode; // point head to the new node
+    this.head = newnode; // point head to the new node
+    this.length++;
   }//end add method
 
   public String get(int index){
     Node currentNode = this.head;
-    String theData = "";
-    int i = 0;
-    while (i < index){
-      if (currentNode.getNext() != null){
+    if (index < this.length){
+      for (int i = 0; i < index; i++){
         currentNode = currentNode.getNext();
-        i++;
-      }//end if-statement
-      else{
-        return null;
-      }//end else-statement
-    }//end while loop
-    return currentNode.getData();
+      }//end for loop
+      return currentNode.getData();
+    }//end if-statement
+    return null;
   }//end get method
+
+  //without the instance variable implemented
+    // while (i < index){
+    //   if (currentNode.getNext() != null){
+    //     currentNode = currentNode.getNext();
+    //     i++;
+    //   }//end if-statement
+    //   else{
+    //     return null;
+    //   }//end else-statement
+    // }//end while loop
+    // return currentNode.getData();
 
   public void set(int index, String value){
     Node currentNode = this.head;
-    int i = 0;
-    while (i < index){
-      if (currentNode.getNext() != null){
+    if (index < this.length){
+      for (int i = 0; i < index; i++){
         currentNode = currentNode.getNext();
-        i++;
-      }//end if-statement
-      else{
-        return;
-      }//end else-statement
-    }//end while loop
-    currentNode.setData(value);
+      }//end for loop
+      currentNode.setData(value);
+    }//end if-statement
+    return;
   }//end set method
+
+  //without the instance variable implemented
+    // while (i < index){
+    //   if (currentNode.getNext() != null){
+    //     currentNode = currentNode.getNext();
+    //     i++;
+    //   }//end if-statement
+    //   else{
+    //     return;
+    //   }//end else-statement
+    // }//end while loop
+    // currentNode.setData(value);
 
   public void insert(int index, String value){
     if (index == 0){
@@ -52,8 +69,8 @@ public class LList{
     }//end if-statement
     else{
       Node currentNode = this.head;
-      int i = 0;
-      while (i < index - 1){
+      int i = 1;
+      while (i < index){
         if (currentNode.getNext() != null){
           currentNode = currentNode.getNext();
           i++;
@@ -65,24 +82,36 @@ public class LList{
       Node newNode = new Node(value);
       newNode.setNext(currentNode.getNext());
       currentNode.setNext(newNode);
+      this.length++;
     }//end else-statement
   }//end insert method
 
   public int search(String key){
     Node currentNode = this.head;
-    int index = 0;
-    if (currentNode.getData().equals(key)){
-      return index;
-    }//end if-statement
-    while (currentNode.getNext() != null){
-      index++;
-      currentNode = currentNode.getNext();
+    for (int i = 0; i < length; i++){
       if (currentNode.getData().equals(key)){
-        return index;
+        return i;
       }//end if-statement
-    }//end while loop
+      currentNode = currentNode.getNext();
+    }//end for loop
     return -1;
   }//end search method
+
+  //without the instance variable implemented
+    // Node currentNode = this.head;
+    // int index = 0;
+    // if (currentNode.getData().equals(key)){
+    //   return index;
+    // }//end if-statement
+    // while (currentNode.getNext() != null){
+    //   index++;
+    //   currentNode = currentNode.getNext();
+    //   if (currentNode.getData().equals(key)){
+    //     return index;
+    //   }//end if-statement
+    // }//end while loop
+    // return -1;
+
 
   public void remove(int index){
     if (index == 0){
@@ -101,7 +130,7 @@ public class LList{
         }
       }//end while loop
       currentNode.setNext(currentNode.getNext().getNext());
-
+      this.length--;
     }//end else-statement
   }//end remove method
 
@@ -115,7 +144,7 @@ public class LList{
     Node tmp;
     tmp = head;
     while (tmp != null){
-      s = s + tmp.getData()+" ";
+      s = s + tmp.getData()+ " ";
       // how can we now move tmp to the next node
       // this is the magic linked list idiom!!!!
       // this moves a pointer to the next node!!!
