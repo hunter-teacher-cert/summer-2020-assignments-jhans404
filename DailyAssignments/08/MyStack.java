0507858c3beafc74aacca2a1fffb31574aff244a
Joshua Hans <mrhansmath@gmail.com>
2020-07-10 13:02:57 -0400

    added search method

--- DailyAssignments/08/MyStack.java
+++ DailyAssignments/08/MyStack.java
@@ -9,9 +9,11 @@ public class MyStack{
       this.arr = new ArrayList<String>();
     }//end constructor method
 
+
     public void push(String data){
         this.arr.add(data);
     }//end push method
+
 
     public String pop(){
       String top = this.arr.get(this.arr.size() - 1);
@@ -19,21 +21,35 @@ public class MyStack{
       return top;
     }//end pop method
 
+
     public String top(){
         return this.arr.get(this.arr.size() - 1);
-    }
+    }//end top class
+
 
     public boolean isEmpty(){
       return this.arr.size() == 0;
     }//end isEmpty method
 
+
     public int size(){
         return this.arr.size();
     }//end size method
 
+
+    public int search(String value){
+      for (int i = this.size() - 1; i >= 0; i--){
+        if (this.arr.get(i).equals(value)){
+          return this.size() - 1 - i;
+        }//end if-statement
+      }//end for loop
+      return -1;
+    }//end search method
+
+
     public String toString(){
       String s = "";
-      for (int i = this.arr.size() - 1; i >= 0; i--){
+      for (int i = this.size() - 1; i >= 0; i--){
         s += this.arr.get(i) + " ";
       }//end for loop
       return s;
