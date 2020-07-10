a01f3c334d098969ba5156fa603db4a73ac5746e
Joshua Hans <mrhansmath@gmail.com>
2020-07-10 11:56:58 -0400

    startup file

---
+++ DailyAssignments/08/MyStack.java
@@ -0,0 +1,31 @@
+public class MyStack{
+    // add your internal data structure here
+
+
+    public myStack(){
+        // add constructor code
+    }
+
+    public void push(String data){
+        // add code to push data on to the stack
+    }
+
+    public String pop(){
+        // add code to remove and return the
+        // item on the top of the stack
+    }
+
+    public String top(){
+        // add code to return but not remove the
+        // item on the top of the stack
+    }
+
+    public boolean isEmpty(){
+        // return true if the stack has no data, false otherwise
+    }
+
+    public int size(){
+        // add code to return the number of items currently on the stack
+    }
+
+}
