8365dd930ac6082ac544cc26c8da3ae5f1b10543
Joshua Hans <mrhansmath@gmail.com>
2020-07-14 09:34:12 -0400

    added comments to constructor

--- DailyAssignments/09/MyStack2.java
+++ DailyAssignments/09/MyStack2.java
@@ -10,21 +10,21 @@ public class MyStack2{
     this.word = s;
     this.stk = new Stack<String>();
 
-    int spaceIndex = s.indexOf(" ");
+    int spaceIndex = s.indexOf(" "); // check to see if there is a space in the word
     int begin = 0;
 
-    if(spaceIndex == -1){
+    if(spaceIndex == -1){ // if there isn't a space, go character by character
       for (int i = 0; i < s.length(); i++){
         stk.push(s.substring(i, i + 1));
       }// end for loop
     }// end if statement
-    else{
-      while (spaceIndex != -1){
-        stk.push(s.substring(begin, spaceIndex));
-        begin = spaceIndex + 1;
-        spaceIndex = s.indexOf(" ", spaceIndex + 1);
+    else{ // split by word case
+      while (spaceIndex != -1){ // while there are still spaces to go through
+        stk.push(s.substring(begin, spaceIndex)); //grab a substring from begin to one index before where the space is
+        begin = spaceIndex + 1; // reassign begin to just after the space found
+        spaceIndex = s.indexOf(" ", spaceIndex + 1); // find the next existance of a space in the String
       }//end while loop
-      stk.push(s.substring(begin));
+      stk.push(s.substring(begin)); // add the last word in the String
     }// end else statement
   }// end constructor method
 
