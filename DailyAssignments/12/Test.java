949490c30e286b235ef31a6811d7ea48f3386ba4
Joshua Hans <mrhansmath@gmail.com>
2020-07-16 13:32:36 -0400

    cleaned up a bit

--- DailyAssignments/12/Test.java
+++ DailyAssignments/12/Test.java
@@ -3,23 +3,18 @@ import java.io.*;
 
 public class Test{
   public static ArrayList<Integer> slice(ArrayList<Integer> al, int lo, int hi) {
-
     ArrayList<Integer> sub = new ArrayList<Integer>();
+
     for (; lo < hi; lo++) {
       sub.add( al.get(lo) );
     }//copy
     return sub;
   }//end slice
 
-  public static ArrayList<Integer> prestoArrayListo(int numItems, int lo, int hi)
-  {
+  public static ArrayList<Integer> prestoArrayListo(int numItems, int lo, int hi){
     ArrayList<Integer> retArr = new ArrayList();
 
     for(int i=0; i<numItems; i++) {
-      //System.out.println(i);  //diagnostic under-the-hood view
-      //retArr.add( Math.random() ); // [0,1)
-      //retArr.add( (int)Math.random() ); // 0
-      //retArr.add( (int)(hi * Math.random()) ); // [0,hi)
       retArr.add( lo + (int)(hi * Math.random()) ); // [lo,lo+hi)
     }//end for loop
     return retArr;
@@ -29,11 +24,11 @@ public class Test{
 
       // int smallIndex;
       // System.out.println("Testing findSmallest");
-      ArrayList<Integer>  slist = prestoArrayListo(25,0,200);
+      ArrayList<Integer>  slist = prestoArrayListo(16,0,200);
       // smallIndex = findSmallest(slist,0,slist.size());
       System.out.println(slist);
       // System.out.printf("Smallest is at slist[%d] and is %d\n",smallIndex,slist.get(smallIndex));
-      System.out.println(slice(slist, 1, 5));
+      System.out.println(slice(slist, 0, 4));
   }// end main
 
 }//end class
